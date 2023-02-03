e61f956773fd7cbc878eb781393397980c7f7cec
DreamFlower14 <sunghh1004@naver.com>
2023-02-03 23:01:33 +0900

    230203

---
+++ src/main/java/kopo/poly/WinterPtApplication.java
@@ -0,0 +1,13 @@
+package kopo.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinterPtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinterPtApplication.class, args);
+    }
+
+}
